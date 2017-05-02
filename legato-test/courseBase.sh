78e6106e2e0f932185f8f8bc06b5503b29e3d035
Bertrand Roussel <bertrand.roussel@cor-net.org>
2017-05-02 07:43:58 -0700

    Checkout legato

--- legato-test/courseBase.sh
+++ legato-test/courseBase.sh
@@ -1,1 +1,15 @@
 docker pull quay.io/legato/virt-x86
+
+# Make repo available
+mkdir -p $HOME/bin
+wget -O $HOME/bin/repo https://storage.googleapis.com/git-repo-downloads/repo
+chmod +x $HOME/bin/repo
+
+export PATH=$HOME/bin:$PATH
+
+# Checkout legato
+mkdir -p workspace
+cd workspace
+repo init -u git://github.com/legatoproject/manifest
+repo sync
+
